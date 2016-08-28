fc02e7a51db2723d6d53a1e4890eecf8740dd98e
cutecoley <manjula.manicka@gmail.com>
2016-08-28 16:23:40 -0700

    completed getElementsByClassName and stringyfyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
   // your code here
+  var getElementsByClassName = function(className) {
+ /* first get the html inclu. head and body store it in a variable x
+ loop through x to find if it has any child
+ if there is no child then check for className
+ if it matches return the tag */
+ var x = document.body;
+ var result = [];
+ function traverse(element, result) {
+   var children = element.children;
+   var multiple = element.className.split(" ");
+   if (multiple.indexOf(className) >= 0) {
+     result.push(element);
+  }
+
+  for (var i = 0; i < children.length; i++) {
+       traverse(children[i], result);
+     }
+  }
+
+
+ traverse(x, result);
+ return result;
+
 };
+
+
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = result || [];
+  if  (obj === null){
+    return 'null';
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean' ){
+    return obj.toString();
+  }
+  if (typeof obj === 'string'){
+      return '"' + obj.toString() + '"'
+  }
+  if (Array.isArray(obj))
+  {
+        _.each(obj, function(ele,index){
+        if (ele !== undefined)
+        result.push(stringifyJSON(ele));
+      })
+      return '[' + result.join(',') + ']';
+ }
+ if (typeof obj === 'object') {
+      _.each(obj, function(value,key){
+        if(value !== undefined && typeof(value) !== 'function'){
+          result.push(stringifyJSON(key) + ':' + stringifyJSON(value));
+        }
+    })
+    return "{" + result.join() + "}";
+  } ;
+
 };
+
+
+
+
